6baec1a324e40ab34feb32ad9b0cd5536ad6b78e
Unknown <jackienorstrom@aol.com>
2019-05-10 07:52:22 -0400

    Master commit.

---
+++ LASDictionaryV4/LASDictionaryV4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LASDictionaryV4
+//
+//  Created by Jackie Norstrom on 5/10/19.
+//  Copyright © 2019 LAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
